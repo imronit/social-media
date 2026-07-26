a03896746bf3e9a691e9c1cd76f1cf0aa00852cc
Ronit Kumar Panda <ronitpanda.rkp@gmail.com>
2026-07-26 11:38:32 +0530

    Refactored

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import Home from './pages/home/home';
-import Login from './pages/login/Login';
-import Profile from './pages/profile/Profile';
-import Register from './pages/register/Register';
-//import PersonIcon from "@mui/icons-material/Person";
+
 function App() {
   return (
     <>
